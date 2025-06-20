d7d58690394aff1c1836e03af4b4b807c42009d8
Ramprasad V <vrprasad@gmail.com>
2025-06-20 10:27:14 +0530

    Added user-api files

--- user-api/src/main/java/com/prasad/userservice/UserApplication.java
+++ user-api/src/main/java/com/prasad/userservice/UserApplication.java
@@ -1,13 +1,13 @@
-package com.prasad.facade;
+package com.prasad.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FacadeApplication {
+public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FacadeApplication.class, args);
+		SpringApplication.run(UserApplication.class, args);
 	}
 
 }
